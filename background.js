cef626280dad9a1c75f5afdb61ee0369522bb1fc
yz <yz_0812@outlook.com>
2024-09-21 16:45:21 +0800

    代码格式化

--- background.js
+++ background.js
@@ -1,63 +1,59 @@
 chrome.runtime.onInstalled.addListener(() => {
   // 创建右键菜单
   chrome.contextMenus.create({
-      id: "addWhitelist",
-      title: "添加到白名单",
-      contexts: ["all"]
+    id: "addWhitelist",
+    title: "添加到白名单",
+    contexts: ["all"],
   });
 
   chrome.contextMenus.create({
-      id: "quickGroup",
-      title: "快速分组",
-      contexts: ["all"]
+    id: "quickGroup",
+    title: "快速分组",
+    contexts: ["all"],
   });
 });
-
 
 // 监听标签页更新事件
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // 检查页面是否已加载完成
-  if (changeInfo.status === 'complete') {
-    
+  if (changeInfo.status === "complete") {
     // 调用 groupTabs 函数
     groupTabs();
   }
 });
 // 关闭事件
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
   console.info("关闭监听");
   // 判断标签页是否是窗口关闭导致的
   if (!removeInfo.isWindowClosing) {
-      // 调用 groupTabs 函数
-      groupTabs();
+    // 调用 groupTabs 函数
+    groupTabs();
   }
 });
 
 // 监听 subdomainToggle 状态的变化
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   if (changes.subdomainEnabled) {
-      const newValue = changes.subdomainEnabled.newValue;
-      ungroupAllTabs();
-      groupTabs();
+    const newValue = changes.subdomainEnabled.newValue;
+    ungroupAllTabs();
+    groupTabs();
   }
 });
 
-
 // 在background script中监听消息
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'quickGroup') {
-      groupTabs(); // 调用你的快速分组逻辑
-      sendResponse({ status: 'Tabs grouped successfully' });
+  if (message.action === "quickGroup") {
+    groupTabs(); // 调用你的快速分组逻辑
+    sendResponse({ status: "Tabs grouped successfully" });
   }
 });
 
 // 白名单存储和读取
 function getWhitelist(callback) {
-  chrome.storage.sync.get(["whitelist"], function(result) {
-      callback(result.whitelist || []);
+  chrome.storage.sync.get(["whitelist"], function (result) {
+    callback(result.whitelist || []);
   });
 }
-
 
 // 读取 subdomainEnabled 的状态
 // function getSubdomainEnabled(callback) {
@@ -71,11 +67,18 @@ function getWhitelist(callback) {
 
 function getSubdomainEnabled() {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get(['subdomainEnabled'], function(result) {
+    chrome.storage.local.get(["subdomainEnabled"], function (result) {
       if (chrome.runtime.lastError) {
-        reject(new Error(`Error getting subdomainEnabled: ${chrome.runtime.lastError}`));
+        reject(
+          new Error(
+            `Error getting subdomainEnabled: ${chrome.runtime.lastError}`
+          )
+        );
       } else {
-        const isEnabled = result.subdomainEnabled !== undefined ? result.subdomainEnabled : false;
+        const isEnabled =
+          result.subdomainEnabled !== undefined
+            ? result.subdomainEnabled
+            : false;
         resolve(isEnabled);
       }
     });
@@ -83,63 +86,57 @@ function getSubdomainEnabled() {
 }
 
 function addToWhitelist(domain) {
-  getWhitelist(function(whitelist) {
-      if (!whitelist.includes(domain)) {
-          whitelist.push(domain);
-          chrome.storage.sync.set({ whitelist: whitelist });
-      }
+  getWhitelist(function (whitelist) {
+    if (!whitelist.includes(domain)) {
+      whitelist.push(domain);
+      chrome.storage.sync.set({ whitelist: whitelist });
+    }
   });
 }
 
 // 处理右键菜单点击
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "addWhitelist") {
-      const url = new URL(tab.url);
-      const domain = url.hostname;
-      addToWhitelist(domain);
-      alert(`${domain} 已添加到白名单`);
+    const url = new URL(tab.url);
+    const domain = url.hostname;
+    addToWhitelist(domain);
+    alert(`${domain} 已添加到白名单`);
   } else if (info.menuItemId === "quickGroup") {
-      groupTabs();
+    groupTabs();
   }
 });
 
-
-
-
-
-
 async function groupTabs() {
-
-  let localIsEnabled = await  getSubdomainEnabled();
+  let localIsEnabled = await getSubdomainEnabled();
   // 获取白名单
   getWhitelist(function (whitelist) {
     chrome.tabs.query({}, function (tabs) {
       let groups = {}; // 用于存储每个域名的标签ID
 
       // 遍历所有标签页，根据域名分组
-      tabs.forEach(tab => {
+      tabs.forEach((tab) => {
         try {
           const url = new URL(tab.url);
 
           // 跳过 Chrome 内置页面，例如 chrome:// 和 about://
           if (url.protocol === "chrome:" || url.protocol === "about:") {
-          //  console.log("Skipping internal Chrome page:", tab.url);
+            //  console.log("Skipping internal Chrome page:", tab.url);
             return; // 跳过内置页面
           }
-          
-          
-            // 不开启子域名
-               // 提取一级域名进行白名单匹配
-                    const domainParts = url.hostname.split('.');
-                    let  topLevelDomain = domainParts;
-                    if(!localIsEnabled){
-                        topLevelDomain = domainParts.slice(-2).join('.'); // 提取一级域名
-                    }
 
-            // 检查是否是 IP 地址
-          const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){2}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-          if( ipRegex.test(url.hostname)){
-            topLevelDomain =  url.hostname;
+          // 不开启子域名
+          // 提取一级域名进行白名单匹配
+          const domainParts = url.hostname.split(".");
+          let topLevelDomain = domainParts;
+          if (!localIsEnabled) {
+            topLevelDomain = domainParts.slice(-2).join("."); // 提取一级域名
+          }
+
+          // 检查是否是 IP 地址
+          const ipRegex =
+            /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){2}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+          if (ipRegex.test(url.hostname)) {
+            topLevelDomain = url.hostname;
           }
           // 如果域名在白名单中，跳过该标签页的分组
           if (whitelist.includes(topLevelDomain)) {
@@ -160,15 +157,15 @@ async function groupTabs() {
       });
 
       // 遍历每个分组的标签
-      Object.keys(groups).forEach(groupName => {
+      Object.keys(groups).forEach((groupName) => {
         const groupTabs = groups[groupName];
 
         // 如果某个分组的标签页数量大于1才进行分组
         if (groupTabs.length > 1) {
-          const tabIds = groupTabs.map(tab => tab.id);
-          
+          const tabIds = groupTabs.map((tab) => tab.id);
+
           // 执行分组操作
-          chrome.tabs.group({ tabIds: tabIds }, groupId => {
+          chrome.tabs.group({ tabIds: tabIds }, (groupId) => {
             if (chrome.runtime.lastError) {
               console.error("Failed to group tabs:", chrome.runtime.lastError);
               return;
@@ -178,11 +175,14 @@ async function groupTabs() {
             const groupTitle = groupName || "新分组";
 
             // 更新分组的标题
-           // 确保 groupId 是有效的
+            // 确保 groupId 是有效的
             if (groupId) {
               chrome.tabGroups.update(groupId, { title: groupTitle }, () => {
                 if (chrome.runtime.lastError) {
-                  console.error("Failed to update tab group title:", chrome.runtime.lastError);
+                  console.error(
+                    "Failed to update tab group title:",
+                    chrome.runtime.lastError
+                  );
                 }
               });
             } else {
@@ -192,19 +192,19 @@ async function groupTabs() {
         }
       });
 
-            // 移出只有一个标签页的分组
-            chrome.tabGroups.query({}, function (tabGroups) {
-              tabGroups.forEach(group => {
-                chrome.tabs.query({ groupId: group.id }, function (groupTabs) {
-                  if (groupTabs.length === 1) {
-                    // 只有一个标签页，将该标签页移出分组
-                    chrome.tabs.ungroup(groupTabs[0].id, function () {
-                  //    console.log("Tab " + groupTabs[0].id + " has been ungrouped from a group with only one tab.");
-                    });
-                  }
-                });
+      // 移出只有一个标签页的分组
+      chrome.tabGroups.query({}, function (tabGroups) {
+        tabGroups.forEach((group) => {
+          chrome.tabs.query({ groupId: group.id }, function (groupTabs) {
+            if (groupTabs.length === 1) {
+              // 只有一个标签页，将该标签页移出分组
+              chrome.tabs.ungroup(groupTabs[0].id, function () {
+                //    console.log("Tab " + groupTabs[0].id + " has been ungrouped from a group with only one tab.");
               });
-            });
+            }
+          });
+        });
+      });
     });
   });
 }
@@ -217,9 +217,12 @@ function ungroupAllTabs() {
         // 如果在分组中，则将其移出分组
         chrome.tabs.ungroup(tab.id, () => {
           if (chrome.runtime.lastError) {
-            console.error(`Error ungrouping tab ${tab.id}:`, chrome.runtime.lastError);
+            console.error(
+              `Error ungrouping tab ${tab.id}:`,
+              chrome.runtime.lastError
+            );
           } else {
-           // console.log(`Tab ${tab.id} has been ungrouped.`);
+            // console.log(`Tab ${tab.id} has been ungrouped.`);
           }
         });
       }
--- popup.js
+++ popup.js
@@ -1,24 +1,22 @@
-document.getElementById('quickGroupButton').addEventListener('click', () => {
-  chrome.runtime.sendMessage({ action: 'quickGroup' });
+document.getElementById("quickGroupButton").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "quickGroup" });
 });
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.getElementById('subdomainToggle');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("subdomainToggle");
 
   // 从 chrome.storage.local 中读取开关状态
-  chrome.storage.local.get(['subdomainEnabled'], function(result) {
-      if (result.subdomainEnabled !== undefined) {
-          toggle.checked = result.subdomainEnabled; // 根据存储的值设置 toggle 的初始状态
-      }
+  chrome.storage.local.get(["subdomainEnabled"], function (result) {
+    if (result.subdomainEnabled !== undefined) {
+      toggle.checked = result.subdomainEnabled; // 根据存储的值设置 toggle 的初始状态
+    }
   });
 
   // 当 toggle 开关状态改变时，将新状态写入 chrome.storage.local
-  toggle.addEventListener('change', function () {
-      const isEnabled = toggle.checked; // 获取当前开关状态
-      chrome.storage.local.set({ subdomainEnabled: isEnabled }, function() {
-        //  console.log('Subdomain toggle state saved:', isEnabled);
-      });
+  toggle.addEventListener("change", function () {
+    const isEnabled = toggle.checked; // 获取当前开关状态
+    chrome.storage.local.set({ subdomainEnabled: isEnabled }, function () {
+      //  console.log('Subdomain toggle state saved:', isEnabled);
+    });
   });
 });
